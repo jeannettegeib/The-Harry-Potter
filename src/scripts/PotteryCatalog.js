43554873122d4fb775ef6b2d1d85bf770ea5d663
Jeannette Geib <jeannettegeib@gmail.com>
2022-06-24 13:03:00 -0400

    work in progress 6-23-22 cracked items filtered and salable items priced

---
+++ src/scripts/PotteryCatalog.js
@@ -0,0 +1,28 @@
+// Define a variable in the module with a value of an empty array. This array will store pottery that will be sold. Do not export this array.
+
+let potteryForSale= [];
+
+// Define and export a function named toSellOrNotToSell that is responsible for determining if a piece of pottery should be sold.// The toSellOrNotToSell function must accept a pottery object as input.
+
+export const toSellOrNotToSell= (paramObject)=>{
+// If the weight of the piece of pottery is greater than, or equal to, 6 then the function must add a price property with a value of 40.// If the weight of the piece of pottery is less than 6 then the function must add a price property with a value of 20.
+paramObject.weight >= 6? paramObject.price=40 : paramObject.price=20;
+
+// If the pottery is not cracked, add the object to the module-level array of items to be sold.
+if (paramObject.cracked === false){ 
+    potteryForSale.push(paramObject)
+}
+
+return potteryForSale;
+
+}
+
+// Define and export a function named usePottery returns a copy of the array of items to be sold. Recall which array method creates a copy of the array.???
+export const usePottery =() =>{
+let pricedPotteryForSale = [];
+pricedPotteryForSale.push(potteryForSale);
+return pricedPotteryForSale;
+}
+
+
+
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,13 @@
 // Imports go first
 import { makePottery } from './PotteryWheel.js'
 import { firePottery } from './Kiln.js'
+import { toSellOrNotToSell } from './PotteryCatalog.js'
 
 // Make 5 pieces of pottery at the wheel
 const mug = makePottery("mug", 8, 4)
 let bowl = makePottery("bowl", 16, 3.5)
 let napkinRing = makePottery("napkin ring", 4, 1.8)
 let plate = makePottery("plate", 15, 1.4)
-
-
 
 // Fire each piece of pottery in the kiln
 const firedMug = firePottery(mug, 2201);
@@ -19,7 +18,13 @@ const firedPlate = firePottery(plate, 2211)
 console.log(firedMug, firedBowl, firedNapkinRing, firedPlate)
 
 // Determine which ones should be sold, and their price
+let arrayOfPotterForSale = [];
+arrayOfPotterForSale = toSellOrNotToSell(firedMug);
+arrayOfPotterForSale = toSellOrNotToSell(firedBowl);
+arrayOfPotterForSale = toSellOrNotToSell(firedNapkinRing);
+arrayOfPotterForSale = toSellOrNotToSell(firedPlate);
 
+console.log(arrayOfPotterForSale)
 
 // Invoke the component function that renders the HTML list
 
